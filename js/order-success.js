87dffa9435525d49d4da86747d3252c54f32a3e3
Ahmet <cicekliticaret98@gmail.com>
2025-04-14 20:18:18 +0300

    Removed submodule cicekli-ic-giyim

--- js/order-success.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const currentUser = JSON.parse(localStorage.getItem("sessionUser"));
-    const greetingElement = document.getElementById("greeting");
-  
-    if (currentUser && greetingElement) {
-      greetingElement.textContent = `Teşekkürler, ${currentUser.name}! Siparişiniz başarıyla oluşturuldu.`;
-    }
-  });
-  
